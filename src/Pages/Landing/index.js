434dd6fb1385ad7d11c592d7a6f243b6530cecba
Medmly20208 <moulaymohamed856@gmail.com>
2022-09-07 18:47:41 +0100

    Adding Slider to Products sections

--- src/Pages/Landing/index.js
+++ src/Pages/Landing/index.js
@@ -2,12 +2,17 @@ import React from "react";
 import Header from "../../Layouts/Header";
 import Main from "./Main";
 import MenProducts from "./MenProducts";
+import WomenProducts from "./WomenProducts";
+import KidsProducts from "./KidsProducts";
+
 const index = () => {
   return (
     <>
       <Header />
       <Main />
       <MenProducts />
+      <WomenProducts />
+      <KidsProducts />
     </>
   );
 };
