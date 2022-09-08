4a5536abe4e98e42fd652a15ab743681984e4717
Medmly20208 <moulaymohamed856@gmail.com>
2022-09-08 23:02:51 +0100

    Added Image onHover Animation in storeItem

--- src/Pages/Landing/StoreItem.js
+++ src/Pages/Landing/StoreItem.js
@@ -2,8 +2,12 @@ import React from "react";
 
 const StoreItem = (props) => {
   return (
-    <div className="flex justify-center items-center  w-[300px] h-80 sm:w-[90%]">
-      <img src={props.Image} alt="clothes store" className="w-full h-full" />
+    <div className="group flex justify-center items-center  w-[300px] h-80 sm:w-[90%] overflow-hidden cursor-pointer">
+      <img
+        src={props.Image}
+        alt="clothes store"
+        className="w-full h-full transition-all duration-700 group-hover:scale-125 "
+      />
       <div className="absolute bg-black text-white w-16 text-center text-bold">
         {props.title}
       </div>
